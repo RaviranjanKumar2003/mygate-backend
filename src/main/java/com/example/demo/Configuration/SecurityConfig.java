c715ba948574db7398ab227c22925865a32b1be4
Raviranjan Kumar <raviranjankumarnd2003@gmail.com>
2026-04-02 11:23:08 +0530

    fix cors for netlify

--- src/main/java/com/example/demo/Configuration/SecurityConfig.java
+++ src/main/java/com/example/demo/Configuration/SecurityConfig.java
@@ -58,7 +58,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://mygateui.netlify.app"
+        ));
+        config.setAllowCredentials(true);
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
